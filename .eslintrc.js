d945db712d5d8e45c1bbc21a696b11d588ef5eb7
hjq <13293349837@163.com>
2020-10-18 22:15:39 +0800

    添加校验

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,10 @@ module.exports = {
     // allow async-await
     'generator-star-spacing': 'off',
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    //"space-before-function-paren":"off",//取消函数前的空格
+    //"object-curly-spacing":"off",//取消括号内的间距一致
+    'indent': 'off'//取消对每行前空格的检测（该换行还是要换的）
+
   }
 }
